4694002f6b6586926ddfd100c0150678c99afb18
elizeumarino <elizeumarino@gmail.com>
2016-09-19 20:05:31 -0300

    Update menumaster.sh

--- menumaster.sh
+++ menumaster.sh
@@ -21,9 +21,12 @@
 #  ---------------------------------------------------------------------
 
 clear
-
-menu ()
-	{
+if [ "`id -u`" != "0" ] ; then
+gksudo $0
+exit
+fi
+menu ()	{
+#Inicio Menu
 
 Escolha=$(zenity  --title="MENU MASTER" --width=600 --height=500 --list \
 --text "Escolha uma opção ! " \
@@ -45,11 +48,28 @@ FALSE "Instalar Conky e Conky Mananger" \
 FALSE "SAIR"
 ); echo $Escolha
 
+Funcao_Escolha
+#Fim Menu
+}
 
+Funcao_Executar () {
+# EXEMPLO PARA REPOSITORIOS
+# COMANDO='sudo add-apt-repository ppa:ricotz/docky'; TITULO='..:: Instalando Repositorio ::..'; TEXTO=' Acidionando Repositorio...... Por favor aguarde...'
 
+# EXEMPLO PARA ATUALIZACAO DE REPOSITORIOS
+# COMANDO='sudo apt-get -y update'; TITULO=' ..:: Atualizando Repositorios ::.. '; TEXTO=' Atualizando Repositorios...... Por favor aguarde...'
 
-	Funcao_Escolha
+# EXEMPLO PARA INSTALACAO DE APLICATIVOS E PROGRAMAS
+# COMANDO='sudo apt-get -y install plank'; TITULO=' ..:: Instalando ::.. '; TEXTO=' Instalando Plank Dock...... Por favor aguarde...'
+
+	Barra_Progresso () {
+		tail -f $0 | zenity --progress --width=400 --height=100 --title "$TITULO" --pulsate --auto-close --auto-kill --text "$TEXTO"
 	}
+
+Barra_Progresso "Aguarde..." & sudo $COMANDO
+killall tail
+zenity --info --width=400 --height=100 --title " ...::: OBRIGADO ! :::..." --text " Obrigado por usar Quick Master Menu Install"
+}
 
 	Funcao_Escolha ()
 	{
@@ -60,9 +80,16 @@ FALSE "SAIR"
 			echo "                    Instalando Java Oracle... "
 			echo "----------------------------------------------------------------------"
 			echo ;
-			#sudo add-apt-repository ppa:webupd8team/java && sudo apt-get update;
+
+			COMANDO='sudo add-apt-repository -y ppa:webupd8team/java && sudo apt-get -y update'; TITULO='..:: Instalando Repositorio ::..'; TEXTO=' Acidionando Repositorio...... Por favor aguarde...'
+			Funcao_Executar
+
+			COMANDO='sudo apt-get -y install oracle-java8-installer'; TITULO=' ..:: Instalando ::.. '; TEXTO=' Instalando Oracle-Java8...... Por favor aguarde...'
+			Funcao_Executar
 			#sudo apt-get install oracle-java8-installer;
-			#sudo apt-get install oracle-java8-set-default;
+			COMANDO='sudo apt-get -y install oracle-java8-set-default'; TITULO=' ..:: Instalando ::.. '; TEXTO=' Instalando Oracle-Java8-set-default...... Por favor aguarde...'
+			Funcao_Executar
+			#sudo apt-get install oracle-java8-set-default;;
 		menu ;;
 
 		"Instalar Terminal Terra")	clear
@@ -81,9 +108,22 @@ FALSE "SAIR"
 			echo "                    Instalando PPA Mananger... "
 			echo "----------------------------------------------------------------------"
 			echo ;
+			# EXEMPLO PARA REPOSITORIOS
+			# COMANDO='sudo add-apt-repository ppa:ricotz/docky'; TITULO='..:: Instalando Repositorio ::..'; TEXTO=' Acidionando Repositorio...... Por favor aguarde...'
+
+			# EXEMPLO PARA ATUALIZACAO DE REPOSITORIOS
+			# COMANDO='sudo apt-get -y update'; TITULO=' ..:: Atualizando Repositorios ::.. '; TEXTO=' Atualizando Repositorios...... Por favor aguarde...'
+
+			# EXEMPLO PARA INSTALACAO DE APLICATIVOS E PROGRAMAS
+			# COMANDO='sudo apt-get -y install plank'; TITULO=' ..:: Instalando ::.. '; TEXTO=' Instalando Plank Dock...... Por favor aguarde...'
+
+			COMANDO='sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager && sudo apt-get -y update'; TITULO='..:: Instalando Repositorio ::..'; TEXTO=' Acidionando Repositorio...... Por favor aguarde...'
+			Funcao_Executar
 			#sudo add-apt-repository ppa:webupd8team/y-ppa-manager
 			#sudo apt-get update
 			#sudo apt-get install y-ppa-manager
+			COMANDO='sudo apt-get -y install y-ppa-manager'; TITULO=' ..:: Instalando ::.. '; TEXTO=' Instalando PPA-Manager... Por favor aguarde...'
+			Funcao_Executar
 		menu ;;
 
 		"Instalar VLC media player")	clear
@@ -92,8 +132,12 @@ FALSE "SAIR"
 			echo "                    Instalando VLC media player"
 			echo "----------------------------------------------------------------------"
 			echo ;
+			COMANDO='sudo add-apt-repository -y ppa:videolan/stable-daily && sudo apt-get -y update'; TITULO='..:: Instalando Repositorio ::..'; TEXTO=' Acidionando Repositorio...... Por favor aguarde...'
+			Funcao_Executar
 			# sudo add-apt-repository ppa:videolan/stable-daily && sudo apt-get update
 			# sudo apt-get dist-upgrade && sudo apt-get install vlc browser-plugin-vlc
+			COMANDO='sudo apt-get -y dist-upgrade && sudo apt-get -y install vlc browser-plugin-vlc'; TITULO=' ..:: Instalando ::.. '; TEXTO=' Instalando VLC Media Player..... Por favor aguarde...'
+			Funcao_Executar
 		menu ;;
 
 		"Instalar Ubuntu Tweak")	clear
@@ -138,6 +182,7 @@ FALSE "SAIR"
 			#sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 			#sudo apt-get update
 			#sudo apt-get install grub-customizer
+			sudo add-apt-repository ppa:danielrichter2007/grub-customizer && sudo apt-get update && sudo apt-get install grub-customizer
 		menu ;;
 
 		"Instalar Midnight Commander")	clear
@@ -164,7 +209,16 @@ FALSE "SAIR"
 			echo "            Instalando Bleachbit- Utilitario de limpeza... "
 			echo "----------------------------------------------------------------------"
 			echo ;
+			#ENTRA=$(zenity --password --title='Identificação do Usuario')
+
+			#ENTRA=$(zenity --entry --title='Identificação do Usuario' --text='Digite a senha para instalação do aplicativo');
+			#SENHA=$(echo $ENTRA | cut -d "|" -f 2)
+
 			#sudo apt-get install bleachbit
+			COMANDO='sudo apt-get -y update'; TITULO=' ..:: Atualizando Repositorios ::.. '; TEXTO=' Atualizando Repositorios...... Por favor aguarde...'
+			Funcao_Executar
+			COMANDO='sudo apt-get -y install bleachbit'; TITULO=' ..:: Instalando ::.. '; TEXTO=' Instalando bleachbit...... Por favor aguarde...'
+			Funcao_Executar
 		menu ;;
 
 	   "Instalar My Wather Indicator - Só funciona no Ubuntu Unity")	clear
@@ -184,20 +238,34 @@ FALSE "SAIR"
 			echo "    Instalando Conky e Conky Manager 2... "
 			echo "----------------------------------------------------------------------"
 			echo ;
+			COMANDO='echo ${SENHA}\n | sudo add-apt-repository ppa:ricotz/docky'; TITULO='..:: Instalando Repositorio ::..'; TEXTO=' Acidionando Repositorio...... Por favor aguarde...'
+			Funcao_Executar
+			COMANDO='echo ${SENHA}\n | sudo apt-get -y update'; TITULO=' ..:: Atualizando Repositorios ::.. '; TEXTO=' Atualizando Repositorios...... Por favor aguarde...'
+			Funcao_Executar
 			# sudo add-apt-repository ppa:teejee2008/ppa
 			# sudo apt-get update
 			# sudo apt-get install conky conky-manager
 		menu ;;
 
 	"SAIR")	clear
-	zenity --info \
---text=" Mais já ta indo? \n Fica mais um pouco, Vai ter Bolo! \n Saindo... Bye... Tchau... :( "
+	zenity --info --title " ...::: Obrigado!!! Bye Bye!!! :::... " --width=400 --height=100 --text=" \n Mais já ta indo?\n Fica mais um pouco, Vai ter Bolo! \o/\n\n Obrigado por Utilizar o Quick Menu Install\n Thanks you for Use Quick Menu Install\n\n Tank you!\n Elizeu Marino  "
 			echo ;
-			echo "----------------------------------------------------------------------"
-			echo "       Mais já ta indo? Fica mais um pouco, Vai ter Bolo! \o/ "
-			echo "                    Saindo... Bye... Tchau... :( "
-			echo "----------------------------------------------------------------------"
+			echo "______________________________________________________________________"
+			echo " "
+			echo "       Mais já ta indo? Fica mais um pouco, "
+			echo "              Vai ter Bolo! \o/ "
+			echo "           Saindo... Bye... Tchau... :( "
+			echo " "
+			echo "       Obrigado por Utilizar o Quick Menu Install"
+			echo "       Thanks you for Use Quick Menu Install "
+			echo " "
+			echo "       Tank you! Elizeu Marino."
+			echo " "
+			echo "______________________________________________________________________"
 			echo ;
+			# zenity --info --title "$TITULO" --text "Nenhum pacote foi escolhido. O script será encerrado, obrigado."
+
+
 		;;
 
 
